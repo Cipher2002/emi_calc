84309313f783f8816b87b5cd24e3dc05dc5a96eb
Ansh Chauhan <96931815+Cipher2002@users.noreply.github.com>
2023-04-02 17:16:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
-function calculateEMI() {
-  const loanAmount = document.getElementById("loan-amount").value;
-  const annualInterestRate = document.getElementById("annual-interest-rate").value;
-  const tenure = document.getElementById("tenure").value;
+// Get references to the input fields and output elements
+const loanAmountInput = document.getElementById("loanAmount");
+const annualInterestRateInput = document.getElementById("annualInterestRate");
+const tenureInput = document.getElementById("tenure");
+const calculateButton = document.getElementById("calculate-button");
+const totalInterestOutput = document.getElementById("totalInterest");
+const totalPaymentOutput = document.getElementById("totalPayment");
+const monthlyEMIOutput = document.getElementById("monthlyEMI");
 
-  const monthlyInterestRate = (annualInterestRate / 12) / 100;
+// Add event listener to the Calculate button
+calculateButton.addEventListener("click", calculateEMI);
+
+// Define the calculateEMI function
+function calculateEMI() {
+  // Get the input values from the user
+  const loanAmount = parseFloat(loanAmountInput.value);
+  const annualInterestRate = parseFloat(annualInterestRateInput.value);
+  const tenure = parseFloat(tenureInput.value);
+
+  // Calculate the EMI
+  const monthlyInterestRate = annualInterestRate / (12 * 100);
   const numberOfPayments = tenure;
-  
-  const emi = (loanAmount * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments)) / (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
-  const totalPayment = emi * numberOfPayments;
+  const EMI = (loanAmount * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments)) / (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
+
+  // Calculate the total payment and total interest to be paid
+  const totalPayment = EMI * numberOfPayments;
   const totalInterest = totalPayment - loanAmount;
 
-  document.getElementById("emi").innerHTML = emi.toFixed(2);
-  document.getElementById("total-payment").innerHTML = totalPayment.toFixed(2);
-  document.getElementById("total-interest").innerHTML = totalInterest.toFixed(2);
+  // Display the results in the output fields
+  totalInterestOutput.innerHTML = totalInterest.toFixed(2);
+  totalPaymentOutput.innerHTML = totalPayment.toFixed(2);
+  monthlyEMIOutput.innerHTML = EMI.toFixed(2);
 }
-
-document.getElementById("loan-amount").addEventListener("change", calculateEMI);
-document.getElementById("annual-interest-rate").addEventListener("change", calculateEMI);
-document.getElementById("tenure").addEventListener("change", calculateEMI);
